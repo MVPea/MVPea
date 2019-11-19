603c9da42a7d4b161344408a08c03094f81a018b
Vivian Quan <vivianquan@Vivians-MacBook-Pro.local>
2019-11-19 14:05:10 -0800

    edits in TV component

--- client/screens/TvScreen.js
+++ client/screens/TvScreen.js
@@ -1,16 +1,9 @@
 import React from 'react';
 import { ScrollView, StyleSheet, Text } from 'react-native';
-import { ExpoLinksView } from '@expo/samples';
 
 export default function TvScreen() {
   return (
     <ScrollView style={styles.container}>
-      {/**
-       * Go ahead and delete ExpoLinksView and replace it with your content;
-       * we just wanted to provide you with some helpful links.
-       */}
-      <ExpoLinksView />
-
       <Text>TV COMPONENT!</Text>
     </ScrollView>
   );
